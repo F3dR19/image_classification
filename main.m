85ae352cac53f4f531555d4f9ff3eea79bb97463
Federico Danieli <federico.danieli@maths.ox.ac.uk>
2016-10-18 16:38:06 +0100

    changed my projection

--- main.m
+++ main.m
@@ -2,14 +2,14 @@ clear all
 close all
 clc
 
-size_train = 10;
-size_test = 10;
+size_train = 1000;
+size_test = 1000;
 
 [ images_train, images_test, labels_train, labels_test ] = read_data( size_train, size_test );
 
-[ images_train, images_test ] = edit_data( images_train, images_test );
+%[ images_train, images_test ] = edit_data( images_train, images_test );
 
-[ reduced_images_train, reduced_images_test ] = projection( images_train, images_test, 15 );
+[ reduced_images_train, reduced_images_test ] = projection( images_train, images_test, 10 );
 
 [ classifications ] = distance( reduced_images_test, reduced_images_train, labels_train, 10 );
 
